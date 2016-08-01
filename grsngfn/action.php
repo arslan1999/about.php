7756954c9c9785eb8d580fc290a8fc8e5628378b
arslan1999 <arslanlev1999@mail.ru>
2016-08-02 03:11:43 +0500

    Update action.php

--- grsngfn/action.php
+++ grsngfn/action.php
@@ -4,13 +4,20 @@ session_start();
 $userData = $_POST['User'];
 
 if(!empty($userData['login']) && !empty($userData['password'])){
+    $file = __DIR__ . '/login.json';
+    $get_data = file_get_contents($file);
+    $date_user = json_decode($get_data, true);
 
-    $_SESSION['pass'] = $userData['password'];
-    $_SESSION['login'] = $userData['login'];
-    header('Location: list.php', true, 301);
+    foreach ($date_user as $k => $v){
+        if ($v['login'] == $userData['login'] && $v['password'] == $userData['password']) {
+            $_SESSION['pass'] = $userData['password'];
+            $_SESSION['login'] = $userData['login'];
+            header('Location: list.php', true, 301) ;
+        }
+    }
 }
 elseif (!empty($userData['login']) && empty($userData['password'])){
-    $_SESSION['pass'] = 0;
+    $_SESSION['pass'] = 02;
     $_SESSION['login'] = $userData['login'];
     header('Location: list.php', true, 301);
 }
